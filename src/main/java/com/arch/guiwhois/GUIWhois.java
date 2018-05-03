4fe5e33fb6c3c19b982a1c4cc754dbdea4f8f1b7
JohZant <38639837+JohZant@users.noreply.github.com>
2018-05-03 17:44:45 +1000

    Initial commit

---
+++ src/main/java/com/arch/guiwhois/GUIWhois.java
@@ -0,0 +1,17 @@
+package com.arch.guiwhois;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GUIWhois extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+
+}
