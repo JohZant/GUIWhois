23859966853f9f55f913544ce9483f6bb3d82f14
JohZant <38639837+JohZant@users.noreply.github.com>
2018-05-03 22:48:12 +1000

    Added in config.yml and added code to generate it when plugin loads of first time

--- src/main/java/com/arch/guiwhois/GUIWhois.java
+++ src/main/java/com/arch/guiwhois/GUIWhois.java
@@ -1,11 +1,14 @@
 package com.arch.guiwhois;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GUIWhois extends JavaPlugin {
 
     @Override
     public void onEnable(){
+        this.saveDefaultConfig();//generates config.yml if it does not exist
 
     }
 
